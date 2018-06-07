582a984d86c5466df4124955858badd100d29db2
Jimmijamma <ale.morichetta@gmail.com>
2018-06-07 12:49:30 +0200

    Telegram Bot initialized with MQTT

--- parseDataset.py
+++ parseDataset.py
@@ -5,8 +5,10 @@ Created on 09 mag 2018
 '''
 import pandas as pd
 import numpy as np
-from sklearn import datasets, linear_model, neural_network
+from sklearn import datasets, linear_model, neural_network, cluster,svm
 from sklearn.metrics import mean_squared_error, r2_score
+from sympy.polys.numberfields import IntervalPrinter
+
 
 if __name__ == '__main__':
 
@@ -33,25 +35,49 @@ if __name__ == '__main__':
     B=np.array(list(map(float,B)))/1000
     '''
     
-    X=mat[:,:4] # Month,Temperature,Humidity,RainQuantity
+    X=mat[:,1:4] # Month,Temperature,Humidity,RainQuantity
     B=mat[:,-1]/1000 # Leaf Wetness
     # Create linear regression object
+    
     regr = neural_network.MLPRegressor(max_iter=20000)
     
-    '''
+    
     regr = neural_network.MLPClassifier(max_iter=20000)
-    B=pd.cut(B,bins=[-np.inf,120,600,np.inf], labels=[0,1,2])
-    B=np.array(B)
+    supp= svm.SVC()
+    
+    '''
+    B_lol=pd.cut(B,bins=[-np.inf,thr,np.inf], labels=[0,1])
+    B_lol=np.array(B_lol)
+    
+    c_0=np.mean(X[B_lol==0],axis=0)
+    c_1=np.mean(X[B_lol==1],axis=0)
+
+    c_2=np.mean(X[B==2],axis=0)
+    c_3=np.mean(X[B==3],axis=0)
+    c_4=np.mean(X[B==4],axis=0)
+    c_5=np.mean(X[B==5],axis=0)
     '''
     
     lim= int(np.shape(B)[0]*0.9)
     X_train=X[:lim]
     X_test=X[lim:]
+    mean_X=np.mean(X_train, axis=0)
+    std_X=np.mean(X_train,axis=0)
+    #X_norm=(X_train-mean_X)/std_X
+    #X_test_norm=(X_test-mean_X)/std_X
+    #print np.mean(X_norm,axis=0)
     y_train=B[:lim]
     y_test=B[lim:]
     
+    clf = linear_model.LinearRegression(normalize=True)
+    clf.fit(X_train, y_train)
+    print clf.predict(X_test)
+    print y_test
     
+        
+
     
+    '''
     # Train the model using the training sets
     regr.fit(X_train, y_train)
     
@@ -76,5 +102,5 @@ if __name__ == '__main__':
         
     print np.mean(error_list)
     print np.std(error_list)
-    
+    '''
     
--- telegramBot.py
+++ telegramBot.py
@@ -3,10 +3,119 @@ Created on 19 mag 2018
 
 @author: jimmijamma
 '''
-import telegram
-from telegram.ext import Updater
 
-TOKEN='610146619:AAFydsAw1I2QTCRI2mHzSEkifekuI3VM5sU'
+from telegram.ext import Updater,CommandHandler,Filters,MessageHandler
+import logging
+import json
+import paho.mqtt.client as PahoMQTT
+import time
 
-updater = Updater(token=TOKEN)
+MIO_ID=578155659
 
+def checklist(chat_id):
+    jf=open('chatIDs.JSON','rb')
+    jo=json.load(jf)
+    jf.close()
+    if chat_id not in jo['ID_list']:
+        jo['ID_list'].append(chat_id)
+        jf=open('chatIDs.JSON','wb')
+        js=json.dumps(jo)
+        jf.write(js)
+        jf.close()
+        return "Congratulations! You just activated IntoTheVine bot! Your ID is %d" %chat_id
+    else:
+        return "Your bot is already activated"
+        
+class TelegramBot(object):
+    '''
+    Class for sending notifications using Telegram
+    '''
+    
+    def __init__(self, clientID):
+        
+        # TelegramBot attributes
+        self.token='610146619:AAFydsAw1I2QTCRI2mHzSEkifekuI3VM5sU'
+        self.updater=Updater(token=self.token)
+        self.dispatcher = self.updater.dispatcher
+        # for managing errors
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+        self.job_queue=self.updater.job_queue
+        
+        # MQTT attributes
+        self.clientID = clientID
+        self._paho_mqtt = PahoMQTT.Client(clientID, False)
+        # register the callback
+        self._paho_mqtt.on_connect = self.myOnConnect
+        self._paho_mqtt.on_message = self.myOnMessageReceived
+        self.topic = 'notify'
+        
+        start_handler = CommandHandler('start', self.msg_start)
+        self.dispatcher.add_handler(start_handler)
+        echo_handler = MessageHandler(Filters.text, self.msg_echo)
+        self.dispatcher.add_handler(echo_handler)
+
+    
+    def start_polling(self):
+        # to start the bot
+        self.updater.start_polling()
+        print "Bot started polling."
+       
+    def mqtt_start (self):
+        #manage connection to broker
+        self._paho_mqtt.connect('broker.hivemq.com', 1883)
+        self._paho_mqtt.loop_start()
+        # subscribe for a topic
+        self._paho_mqtt.subscribe(self.topic, 2)
+
+    def mqtt_stop (self):
+        self._paho_mqtt.unsubscribe(self.topic)
+        self._paho_mqtt.loop_stop()
+        self._paho_mqtt.disconnect()
+        
+    def msg_start(self, bot, update):
+        chat_id=update.message.chat_id
+        jf=open('chatIDs.JSON','rb')
+        jo=json.load(jf)
+        jf.close()
+        if chat_id not in jo['ID_list']:
+            jo['ID_list'].append(chat_id)
+            jf=open('chatIDs.JSON','wb')
+            js=json.dumps(jo)
+            jf.write(js)
+            jf.close()
+            text='Congratulations! You just activated IntoTheVine bot! Your ID is %d' %chat_id
+        else:
+            text="Your bot is already activated"
+        bot.send_message(chat_id=update.message.chat_id, text=text)
+        
+    def msg_echo(self,bot, update):
+        bot.send_message(chat_id=update.message.chat_id, text='Sorry, this is not a command. All commands start with \'/\'')
+        print "%d sent a message" %update.message.chat_id
+
+    def myOnConnect (self, paho_mqtt, userdata, flags, rc):
+        print ("Connected to message broker with result code: "+str(rc))
+
+    def myOnMessageReceived (self, paho_mqtt , userdata, msg):
+        # A new message is received
+        chat_id= msg.payload
+        def callback_now(bot,job,chat_id=chat_id,text='ALERT!'):
+            bot.send_message(chat_id=chat_id, text=text)
+        self.job_queue.run_once(callback_now, when=0)
+
+
+
+
+if __name__ == '__main__':
+    
+    botty=TelegramBot('jimmijamma')
+    botty.start_polling()
+    botty.mqtt_start()
+    
+    '''
+    jf=open('chatIDs.JSON','w')
+    data={}
+    data['ID_list']=[]
+    js=json.dumps(data)
+    jf.write(js)
+    jf.close()
+    '''
